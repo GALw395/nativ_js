dc34d385052bf9e118f0ddd5200552828b8d508e
GALw395 <galw395@mail.ru>
2024-06-12 16:37:36 +0300

    08 - Associative Array

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 
 import './App.css';
-import { Component, ManPropsType } from './Component';
+import { Component } from './Component';
 
 function App() {
   
-let man: ManPropsType = {
-  name: 'Lera',
-  age: 24,
-  lessons: [{ title: '1' }, { title: '2' }]
-}
-
-
-
-
   return (
-    <Component age={man.age} name={man.name} lessons={man.lessons}/>
+    <Component />
   )
 }
 
--- src/Component.tsx
+++ src/Component.tsx
@@ -10,19 +10,58 @@ export type ManPropsType = {
   lessons: LessonsPropsType[]
 }
 
+let user = {
+  name: "Lera",
+  age: 22,
+  address: {
+    city: {
+      title: "Minsk"
+    }
+  }
+}
+
+let users = ["Lera", "Lera2", "Lera3", "Lera4"]
 
 
+let userObj = {
+  "0": "Lera0",
+  "1": "Lera1",
+  "2": "Lera2",
+  "3": "Lera3"
+}
 
-export const Component = (props: ManPropsType) => {
-  const {name, age, lessons} = props
+
+let obj1 = {
+  "101": { id: 101, name: "Lera0" },
+  "3245": { id: 3245, name: "Lera1" },
+  "1545134": { id: 1545134, name: "Lera2" },
+  "11201": { id: 11201, name: "Lera3" },
+}
+
+let newUser = { id: 52342345, name: "Lexa" }
+
+export const Component = () => {
 
   return (
     <div>
-      {name}
-      {age}
-      {lessons.map((el, index) => {
-        return  <li key={index}>{el.title}</li> 
-      })}
+      <div>
+        {user.address.city.title}
+      </div>
+      <div>
+        {user['address']['city']['title']}
+      </div>
+      <div>
+        {userObj['3']}
+      </div>
+      <div>
+        {Object.keys(userObj)}
+      </div>
+      
+      <div>
+        {Object.values(userObj)}
+      </div>
+
+
     </div>
   )
 }
